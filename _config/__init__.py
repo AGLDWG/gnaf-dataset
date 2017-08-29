0fb80b7a2e25e79eca1741eb787be43f9aa4c77b
Joseph Abhayaratna <Joseph.Abhayaratna@psma.com.au>
2017-08-29 22:24:47 +1000

    Added Street Locality Alias

--- _config/__init__.py
+++ _config/__init__.py
@@ -9,11 +9,14 @@ URI_ADDRESS_INSTANCE_BASE = 'http://transport.data.gov.au/id/address/'
 URI_ADDRESS_SITE_CLASS = 'http://reference.data.gov.au/def/ont/gnaf#AddressSite'
 URI_ADDRESS_SITE_INSTANCE_BASE = 'http://transport.data.gov.au/id/addressSite/'
 
-URI_ADDRESS_SITE_GEOCODE_CLASS = 'http://reference.data.gov.au/def/ont/gnaf#AddressSite'
-URI_ADDRESS_SITE_GEOCODE_INSTANCE_BASE = 'http://transport.data.gov.au/id/addressSite/'
+URI_ADDRESS_SITE_GEOCODE_CLASS = 'http://reference.data.gov.au/def/ont/gnaf#AddressSiteGeocode'
+URI_ADDRESS_SITE_GEOCODE_INSTANCE_BASE = 'http://transport.data.gov.au/id/addressSiteGeocode/'
 
 URI_STREET_CLASS = 'http://reference.data.gov.au/def/ont/gnaf#Street'
 URI_STREET_INSTANCE_BASE = 'http://transport.data.gov.au/id/street/'
+
+URI_STREET_LOCALITY_ALIAS_CLASS = 'http://reference.data.gov.au/def/ont/gnaf#StreetLocalityAlias'
+URI_STREET_LOCALITY_ALIAS_INSTANCE_BASE = 'http://transport.data.gov.au/id/streetLocalityAlias/'
 
 URI_LOCALITY_CLASS = 'http://reference.data.gov.au/def/ont/gnaf#Locality'
 URI_LOCALITY_INSTANCE_BASE = 'http://transport.data.gov.au/id/locality/'
