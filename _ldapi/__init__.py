b87d7bdf0afdde833ebd6c853edd33157eb4c7b2
Nicholas Car <nicholas.car@ga.gov.au>
2017-09-07 13:15:28 +1000

    minor file naming changes

--- _ldapi/__init__.py
+++ _ldapi/__init__.py
@@ -190,12 +190,12 @@ class LDAPI:
     def get_classes_views_formats():
         """
         Caches the graph_classes JSON file in memory
-        :return: a Python object parsed from the classes_views_formats.json file
+        :return: a Python object parsed from the views_formats.json file
         """
         cache = SimpleCache()
         cvf = cache.get('classes_views_formats')
         if cvf is None:
-            cvf = json.load(open(join(dirname(dirname(__file__)), 'controller', 'classes_views_formats.json')))
+            cvf = json.load(open(join(dirname(dirname(__file__)), 'controller', 'views_formats.json')))
             # times out never (i.e. on app startup/shutdown)
             cache.set('classes_views_formats', cvf)
         return cvf
