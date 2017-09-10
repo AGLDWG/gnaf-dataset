41453fe8fc1c367d9748f39589f595e7d764add1
Nicholas Car <nicholas.car@ga.gov.au>
2017-09-10 19:25:26 +1000

    updates to load scripts

--- _load/authority_codes_load.sql
+++ _load/authority_codes_load.sql
@@ -1,9 +1,9 @@
 COPY gnaf.address_alias_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_ADDRESS_ALIAS_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
 COPY gnaf.address_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_ADDRESS_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
 COPY gnaf.flat_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_FLAT_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
-COPY gnaf.geocode_reliability_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_GEOCODED_LEVEL_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
-COPY gnaf.geocode_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_GEOCODE_RELIABILITY_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
-COPY gnaf.geocoded_level_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_GEOCODE_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
+COPY gnaf.geocode_reliability_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_GEOCODE_RELIABILITY_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
+COPY gnaf.geocode_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_GEOCODE_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
+COPY gnaf.geocoded_level_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_GEOCODED_LEVEL_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
 COPY gnaf.level_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_LEVEL_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
 COPY gnaf.locality_alias_type_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_LOCALITY_ALIAS_TYPE_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
 COPY gnaf.locality_class_aut FROM '{PSV_FOLDERS_DIR}Authority Code/Authority_Code_LOCALITY_CLASS_AUT_psv.psv' WITH (format csv, header true, delimiter E'|', NULL '');
