0414f17c1b53a450e678a768b7cf42fcfc3c3be6
Nicholas Car <nicholas.car@ga.gov.au>
2017-09-10 19:45:12 +1000

    updates to load scripts 2

--- _load/run_sql.sh
+++ _load/run_sql.sh
@@ -1,13 +1,7 @@
-#!/usr/bin/env bash
+# in psql as postgres superuser
 
-echo "running SQL scripts"
-psql -U gnaf -d gnaf -a -f tables_create.sql
-echo "tables created"
-psql -U gnaf -d gnaf -a -f address_view_create.sql
-echo "address view created"
-psql -U gnaf -d gnaf -a -f authority_codes_load.sql
-echo "authority codes loaded"
-psql -U gnaf -d gnaf -a -f standard_tables_load.sql
-echo "standard tables loaded"
-psql -U gnaf -d gnaf -a -f add_fk_constraints.sql
-echo "foreign key constraints added"
+\i tables_create.sql
+\i address_view_create.sql
+\i authority_codes_load.sql
+\i standard_tables_load.sql
+\i add_fk_constraints.sql
