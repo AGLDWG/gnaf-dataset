da7c30348db84a31b494f253e73f280840d03ff4
Nicholas Car <nicholas.car@csiro.au>
2018-05-03 21:55:58 +1000

    generating Locality RDF

--- graph_builder.py
+++ graph_builder.py
@@ -1,21 +1,36 @@
 import logging
 import _config as config
 import model.address
+import model.locality
 
 
 def get_state_addresses(state_pid):
     cursor = config.get_db_cursor()
     s = '''SELECT address_detail_pid 
-        FROM gnaf.address_detail 
-        WHERE locality_pid 
-        IN (SELECT locality_pid FROM gnaf.locality WHERE state_pid = '{}') 
-        ORDER BY address_detail_pid;'''.format(state_pid)
+           FROM gnaf.address_detail 
+           WHERE locality_pid 
+           IN (SELECT locality_pid FROM gnaf.locality WHERE state_pid = '{}') 
+           ORDER BY address_detail_pid;'''.format(state_pid)
     cursor.execute(s)
 
     with open('addresses_state_{}.txt'.format(state_pid), 'w') as f:
         for row in cursor.fetchall():
             r = config.reg(cursor, row)
             f.write(str(r.address_detail_pid) + '\n')
+
+
+def get_localities_in_state(state_pid):
+    cursor = config.get_db_cursor()
+    s = '''SELECT locality_pid 
+           FROM gnaf.locality 
+           WHERE state_pid = '{}'
+           ORDER BY locality_pid;'''.format(state_pid)
+    cursor.execute(s)
+
+    with open('localities_state_{}.txt'.format(state_pid), 'w') as f:
+        for row in cursor.fetchall():
+            r = config.reg(cursor, row)
+            f.write(str(r.locality_pid) + '\n')
 
 
 def run(index_file, data_file_count):
@@ -57,8 +72,49 @@ def run(index_file, data_file_count):
             logging.log(logging.INFO, 'Last accessed Address: ' + a)
 
 
+def run_localities(locality_index_file, data_file_count):
+    logging.basicConfig(filename='graph_builder.log',
+                        level=logging.DEBUG,
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        format='%(asctime)s %(message)s')
+
+    '''
+    - For each Address in the Address Register,
+    - Create an Address class instance using the ID
+    - Serialise that to a file
+    '''
+    DATA_FILE_LENGTH_MAX = 100000
+
+    cursor = config.get_db_cursor()
+
+    data_file_stem = 'data-locality-'
+    # for idx, addr in enumerate(cursor.fetchall()):
+    for idx, addr in enumerate(open(locality_index_file, 'r').readlines()):
+        a = addr.strip()
+        try:
+            # record the Address being processed in case of failure
+            # every DATA_FILE_LENGTH_MAXth URI, create a new destination file
+            if (idx + 1) % DATA_FILE_LENGTH_MAX == 0:
+                data_file_count += 1
+            with open(data_file_stem + str(data_file_count).zfill(4) + '.nt', 'a') as fl:
+                rdf = model.locality.LocalityRenderer(a, db_cursor=cursor)\
+                    .export_rdf(view='gnaf', format='text/n3').decode('utf-8')
+                print(rdf)
+                fl.write(
+                    rdf
+                )
+        except Exception as e:
+            logging.log(logging.DEBUG, 'address ' + a, e)
+            print('locality ' + a + '\n')
+            print(e)
+            with open('faulty_locality.log', 'a') as f:
+                f.write(addr + '\n')
+        finally:
+            logging.log(logging.INFO, 'Last accessed Locality: ' + a)
+
+
 if __name__ == '__main__':
-    run('addresses_state_8.txt', '1')
+    #run('addresses_state_8.txt', '1')
     # get_state_addresses('1')
     # get_state_addresses('2')
     # get_state_addresses('3')
@@ -68,3 +124,10 @@ if __name__ == '__main__':
     # get_state_addresses('7')
     # get_state_addresses('8')
     # get_state_addresses('9')
+    # get_localities_in_state(8)
+    run_localities('localities_state_8.txt', 1)
+
+    # cursor = config.get_db_cursor()
+    # loc = model.locality.LocalityRenderer('ACT101', db_cursor=cursor)
+    # print(loc.export_rdf(view='gnaf', format='text/n3').decode('utf-8'))
+
